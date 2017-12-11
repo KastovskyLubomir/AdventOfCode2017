d45803c19e7be6a162221a22aae06ff7c28aa724
Lubomir Kastovsky <lubomir.kastovsky@avast.com>
2017-12-11 11:40:38 +0100

    solution for event011

--- event011/event011/main.swift
+++ event011/event011/main.swift
@@ -47,65 +47,95 @@ let data = fileManager.contents(atPath: filePath)
 let str: String = String(data: data!, encoding: String.Encoding.utf8)!
 
 // cut off last character, usualy "\n"
-//let inStr = str[...str.index(before:str.index(before: str.endIndex))]
 let inStr = str.dropLast()
 var strArr = inStr.components(separatedBy: [","])
+print(strArr.count)
 
-func getDistance(array: Array<String>) -> Int {
-    
-    let counts = array.reduce(into: [:]) { counts, word in counts[word, default: 0] += 1 }
-    print(counts)
-    var x = 0 //(counts["s"]! - counts["n"]!)
-    var y = 0 //(counts["ne"]! - counts["sw"]!)
-    var z = 0 //(counts["se"]! - counts["nw"]!)
-    
-    if(counts["s"]! > counts["n"]!) {
-        x = (counts["s"]! - counts["n"]!)
-    }
-    else {
-        x = (counts["n"]! - counts["s"]!)
-    }
-    
-    if(counts["ne"]! > counts["sw"]!) {
-        y = (counts["ne"]! - counts["sw"]!)
-    }
-    else {
-        y = (counts["sw"]! - counts["ne"]!)
-    }
-    
-    if(counts["se"]! > counts["nw"]!) {
-        z = (counts["se"]! - counts["nw"]!)
-    }
-    else {
-        z = (counts["nw"]! - counts["se"]!)
-    }
-    
-    print(x)
-    print(y)
-    print(z)
-    
-    if(counts["se"]!+counts["ne"]!) > (counts["sw"]!+counts["nw"]!) {
-        if counts["s"]! > counts["n"]! {
-            return ()
-        }
-    }
-    
-    return (z+x+(y-x))
+func distanceFromCounts(counts: Dictionary<String, Int>) -> Int {
+
+	var x = 0
+	var y = 0
+	var z = 0
+
+	if(counts["s"]! > counts["n"]!) {
+		x = (counts["s"]! - counts["n"]!)
+	}
+	else {
+		x = (counts["n"]! - counts["s"]!)
+	}
+
+	if(counts["ne"]! > counts["sw"]!) {
+		y = (counts["ne"]! - counts["sw"]!)
+	}
+	else {
+		y = (counts["sw"]! - counts["ne"]!)
+	}
+
+	if(counts["se"]! > counts["nw"]!) {
+		z = (counts["se"]! - counts["nw"]!)
+	}
+	else {
+		z = (counts["nw"]! - counts["se"]!)
+	}
+
+	if(counts["se"]!+counts["ne"]!) > (counts["sw"]!+counts["nw"]!) {
+		if counts["n"]! > counts["s"]! {
+			if(x>z) {
+				return (y+x)
+			}
+			return (y+z)
+		}
+		else {
+			if (x > y) {
+				return (z+x)
+			}
+			return (z+y)
+		}
+	}
+	else {
+		if counts["n"]! > counts["s"]! {
+			if(x>y) {
+				return (z+(x-y))
+			}
+			return (z+(y-x))
+		}
+		else {
+			if (x > z) {
+				return (y+(x-z))
+			}
+			return (y+(z-x))
+		}
+	}
+
 }
 
-func biggestDistance(array: Array<String>) -> Int {
-    for i in
-    return 0
+func getMaxDistance(array: Array<String>) -> Int {
+	var counts = [String: Int]()
+	counts["n"] = 0
+	counts["s"] = 0
+	counts["se"] = 0
+	counts["ne"] = 0
+	counts["sw"] = 0
+	counts["nw"] = 0
+	var max = 0
+	for s in array {
+		counts[s]! += 1
+		let dist = distanceFromCounts(counts: counts)
+			if (dist > max) {
+				max = dist
+			}
+	}
+	return max
 }
 
+print(distanceFromCounts(counts: strArr.reduce(into: [:]) { counts, word in counts[word, default: 0] += 1 }))
 
-print(getDistance(array: strArr))
-
-
-
-
-
-
+let start = DispatchTime.now() // <<<<<<<<<< Start time
+print(getMaxDistance(array: strArr))
+let end = DispatchTime.now()   // <<<<<<<<<<   end time
+let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
+let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
+print("Time to evaluate problem : \(timeInterval) seconds")
 
 
 
