917f48da306263343b44d9d3262578f673794796
Lubomir Kastovsky <lubomir.kastovsky@avast.com>
2017-12-13 13:11:05 +0100

    solution for event 13

--- event012/event012/main.swift
+++ event012/event012/main.swift
@@ -6,6 +6,55 @@
 //  Copyright © 2017 Lubomír Kaštovský. All rights reserved.
 //
 
+/*
+--- Day 12: Digital Plumber ---
+
+Walking along the memory banks of the stream, you find a small village that is experiencing a little confusion: some programs can't communicate with each other.
+
+Programs in this village communicate using a fixed system of pipes. Messages are passed between programs using these pipes, but most programs aren't connected to each other directly. Instead, programs pass messages between each other until the message reaches the intended recipient.
+
+For some reason, though, some of these messages aren't ever reaching their intended recipient, and the programs suspect that some pipes are missing. They would like you to investigate.
+
+You walk through the village and record the ID of each program and the IDs with which it can communicate directly (your puzzle input). Each program has one or more programs with which it can communicate, and these pipes are bidirectional; if 8 says it can communicate with 11, then 11 will say it can communicate with 8.
+
+You need to figure out how many programs are in the group that contains program ID 0.
+
+For example, suppose you go door-to-door like a travelling salesman and record the following list:
+
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+In this example, the following programs are in the group that contains program ID 0:
+
+Program 0 by definition.
+Program 2, directly connected to program 0.
+Program 3 via program 2.
+Program 4 via program 2.
+Program 5 via programs 6, then 4, then 2.
+Program 6 via programs 4, then 2.
+Therefore, a total of 6 programs are in this group; all but program 1, which has a pipe that connects it to itself.
+
+How many programs are in the group that contains program ID 0?
+
+Your puzzle answer was 283.
+
+--- Part Two ---
+
+There are more programs than just the ones in the group containing program ID 0. The rest of them have no way of reaching that group, and still might have no way of reaching each other.
+
+A group is a collection of programs that can all communicate via pipes either directly or indirectly. The programs you identified just a moment ago are all part of the same group. Now, they would like you to determine the total number of groups.
+
+In the example above, there were 2 groups: one consisting of programs 0,2,3,4,5,6, and the other consisting solely of program 1.
+
+How many groups are there in total?
+
+Your puzzle answer was 195.
+*/
+
 import Foundation
 
 func readLinesRemoveEmpty(str: String) -> Array<String> {
@@ -37,8 +86,14 @@ func getStringBytes(str:String) -> Array<UInt8> {
 
 func arrayUInt8ToArrayInt(array: Array<UInt8>) -> Array<Int> {
     var result = [Int]()
+	for i in array {
+		result.append(i.distance(to: UInt8(0)))
+	}
     return result
 }
+
+let uint8Array : Array<UInt8> = [1,2,3,4,255]
+print(arrayUInt8ToArrayInt(array: uint8Array))
 
 let fileManager = FileManager.default
 let fileDir: String = fileManager.currentDirectoryPath
@@ -48,13 +103,12 @@ let str: String = String(data: data!, encoding: String.Encoding.utf8)!
 
 let inStrArr = readLinesRemoveEmpty(str: str)
 
-func createDirectory(array: Array<String>) -> Dictionary<Int,[Int]> {
+func createDictionary(array: Array<String>) -> Dictionary<Int,[Int]> {
     var dict = [Int: [Int]]()
     for s in array {
         let args = s.components(separatedBy: [" ", ","])
         let key = args[0]
         let value = args[2...]
-        print("key: " + key + " value: " + value.description)
         var intValue = [Int]()
         for st in value {
             if !st.isEmpty {
@@ -66,46 +120,47 @@ func createDirectory(array: Array<String>) -> Dictionary<Int,[Int]> {
     return dict
 }
 
+func groupSize(dict:Dictionary<Int,[Int]>, forKey: Int) -> (size: Int, newDict: Dictionary<Int, [Int]>) {
+	var finished = false
+	var dict2 = dict
 
-let dict = createDirectory(array: inStrArr)
-print(dict)
+	var intArr = dict2[forKey]!
+	dict2.removeValue(forKey: forKey)
 
-var dict2 = dict
-var finished = false
-var groups = 0
-var intArr = [Int]()
-
-while dict2.count > 0 {
-    
-    for i in Array(0...10000) {
-        if( dict2[i] != nil) {
-            intArr = dict2[i]!
-            dict2.removeValue(forKey: i)
-            break;
-        }
-    }
-    var finished = false
-    
-    while !finished {
-        var newArr = [Int]()
-        let size = intArr.count
-        for i in intArr {
-            if(dict2[i] != nil) {
-                newArr += dict2[i]!
-                dict2[i] = nil
-            }
-        }
-        print(intArr)
-        print(newArr)
-        intArr += newArr
-        print(intArr.description + "\n")
-        if (intArr.count == size) {
-            finished = true
-        }
-    }
-    print(intArr.count)
-    print(Set(intArr).count)
-    groups += 1
+	var newArr = [Int]()
+	while !finished {
+		newArr.removeAll()
+		let size = intArr.count
+		for i in intArr {
+			if(dict2[i] != nil) {
+				newArr += dict2[i]!
+				dict2[i] = nil
+			}
+		}
+		intArr += newArr
+		if (intArr.count == size) {
+			finished = true
+		}
+	}
+	return (Set(intArr).count, dict2)
 }
 
-print(groups)
+func numberOfGroups(dict:Dictionary<Int,[Int]>) -> Int {
+	var dict2 = dict
+	var groups = 0
+	var key = 0
+	while dict2.count > 0 {
+		let result = groupSize(dict: dict2, forKey: key)
+		dict2 = result.newDict
+		if !dict2.isEmpty {
+			key = dict2.keys[dict2.startIndex]
+		}
+		groups += 1
+	}
+	return groups
+}
+
+
+let dict = createDictionary(array: inStrArr)
+print(groupSize(dict: dict, forKey: 0).size)
+print(numberOfGroups(dict: dict))
---
+++ event013/event013/main.swift
@@ -0,0 +1,367 @@
+//
+//  main.swift
+//  event013
+//
+//  Created by Lubomir Kastovsky on 13/12/2017.
+//  Copyright © 2017 Avast. All rights reserved.
+//
+
+/*
+--- Day 13: Packet Scanners ---
+
+You need to cross a vast firewall. The firewall consists of several layers, each with a security scanner that moves back and forth across the layer. To succeed, you must not be detected by a scanner.
+
+By studying the firewall briefly, you are able to record (in your puzzle input) the depth of each layer and the range of the scanning area for the scanner within it, written as depth: range. Each layer has a thickness of exactly 1. A layer at depth 0 begins immediately inside the firewall; a layer at depth 1 would start immediately after that.
+
+For example, suppose you've recorded the following:
+
+0: 3
+1: 2
+4: 4
+6: 4
+This means that there is a layer immediately inside the firewall (with range 3), a second layer immediately after that (with range 2), a third layer which begins at depth 4 (with range 4), and a fourth layer which begins at depth 6 (also with range 4). Visually, it might look like this:
+
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+Within each layer, a security scanner moves back and forth within its range. Each security scanner starts at the top and moves down until it reaches the bottom, then moves up until it reaches the top, and repeats. A security scanner takes one picosecond to move one step. Drawing scanners as S, the first few picoseconds look like this:
+
+
+Picosecond 0:
+0   1   2   3   4   5   6
+[S] [S] ... ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+Picosecond 1:
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+Picosecond 2:
+0   1   2   3   4   5   6
+[ ] [S] ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+Picosecond 3:
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] ... [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[S]     [S]
+Your plan is to hitch a ride on a packet about to move through the firewall. The packet will travel along the top of each layer, and it moves at one layer per picosecond. Each picosecond, the packet moves one layer forward (its first move takes it into layer 0), and then the scanners move one step. If there is a scanner at the top of the layer as your packet enters it, you are caught. (If a scanner moves into the top of its layer while you are there, you are not caught: it doesn't have time to notice you before you leave.) If you were to do this in the configuration above, marking your current position with parentheses, your passage through the firewall would look like this:
+
+Initial state:
+0   1   2   3   4   5   6
+[S] [S] ... ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+Picosecond 0:
+0   1   2   3   4   5   6
+(S) [S] ... ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+( ) [ ] ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+
+Picosecond 1:
+0   1   2   3   4   5   6
+[ ] ( ) ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] (S) ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+
+Picosecond 2:
+0   1   2   3   4   5   6
+[ ] [S] (.) ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [ ] (.) ... [ ] ... [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[S]     [S]
+
+
+Picosecond 3:
+0   1   2   3   4   5   6
+[ ] [ ] ... (.) [ ] ... [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[S]     [S]
+
+0   1   2   3   4   5   6
+[S] [S] ... (.) [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[ ]             [S]     [S]
+[ ]     [ ]
+
+
+Picosecond 4:
+0   1   2   3   4   5   6
+[S] [S] ... ... ( ) ... [ ]
+[ ] [ ]         [ ]     [ ]
+[ ]             [S]     [S]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [ ] ... ... ( ) ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+
+Picosecond 5:
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] (.) [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [S] ... ... [S] (.) [S]
+[ ] [ ]         [ ]     [ ]
+[S]             [ ]     [ ]
+[ ]     [ ]
+
+
+Picosecond 6:
+0   1   2   3   4   5   6
+[ ] [S] ... ... [S] ... (S)
+[ ] [ ]         [ ]     [ ]
+[S]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] ... ( )
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+In this situation, you are caught in layers 0 and 6, because your packet entered the layer when its scanner was at the top when you entered it. You are not caught in layer 1, since the scanner moved into the top of the layer once you were already there.
+
+The severity of getting caught on a layer is equal to its depth multiplied by its range. (Ignore layers in which you do not get caught.) The severity of the whole trip is the sum of these values. In the example above, the trip severity is 0*3 + 6*4 = 24.
+
+Given the details of the firewall you've recorded, if you leave immediately, what is the severity of your whole trip?
+
+Your puzzle answer was 2160.
+
+--- Part Two ---
+
+Now, you need to pass through the firewall without being caught - easier said than done.
+
+You can't control the speed of the packet, but you can delay it any number of picoseconds. For each picosecond you delay the packet before beginning your trip, all security scanners move one step. You're not in the firewall during this time; you don't enter layer 0 until you stop delaying the packet.
+
+In the example above, if you delay 10 picoseconds (picoseconds 0 - 9), you won't get caught:
+
+State after delaying:
+0   1   2   3   4   5   6
+[ ] [S] ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+Picosecond 10:
+0   1   2   3   4   5   6
+( ) [S] ... ... [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+( ) [ ] ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+
+Picosecond 11:
+0   1   2   3   4   5   6
+[ ] ( ) ... ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[S] (S) ... ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+
+Picosecond 12:
+0   1   2   3   4   5   6
+[S] [S] (.) ... [S] ... [S]
+[ ] [ ]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [ ] (.) ... [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+
+Picosecond 13:
+0   1   2   3   4   5   6
+[ ] [ ] ... (.) [ ] ... [ ]
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [S] ... (.) [ ] ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+
+Picosecond 14:
+0   1   2   3   4   5   6
+[ ] [S] ... ... ( ) ... [ ]
+[ ] [ ]         [ ]     [ ]
+[S]             [S]     [S]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [ ] ... ... ( ) ... [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[S]     [S]
+
+
+Picosecond 15:
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] (.) [ ]
+[S] [S]         [ ]     [ ]
+[ ]             [ ]     [ ]
+[S]     [S]
+
+0   1   2   3   4   5   6
+[S] [S] ... ... [ ] (.) [ ]
+[ ] [ ]         [ ]     [ ]
+[ ]             [S]     [S]
+[ ]     [ ]
+
+
+Picosecond 16:
+0   1   2   3   4   5   6
+[S] [S] ... ... [ ] ... ( )
+[ ] [ ]         [ ]     [ ]
+[ ]             [S]     [S]
+[ ]     [ ]
+
+0   1   2   3   4   5   6
+[ ] [ ] ... ... [ ] ... ( )
+[S] [S]         [S]     [S]
+[ ]             [ ]     [ ]
+[ ]     [ ]
+Because all smaller delays would get you caught, the fewest number of picoseconds you would need to delay to get through safely is 10.
+
+What is the fewest number of picoseconds that you need to delay the packet to pass through the firewall without being caught?
+
+Your puzzle answer was 3907470.
+*/
+
+import Foundation
+
+func readLinesRemoveEmpty(str: String) -> Array<String> {
+	var x = str.components(separatedBy: ["\n"])
+	for i in x.indices {
+		if x[i].isEmpty {
+			x.remove(at: i)
+		}
+	}
+	return x
+}
+
+// input: "1,2,3,4,5"
+func stringNumArrayToArrayOfInt(input:String, separators: CharacterSet) -> Array<Int> {
+	var result = [Int]()
+	let lenArrStr = input.components(separatedBy: separators)
+	for s in lenArrStr {
+		if(!s.isEmpty) {
+			result.append(Int(s)!)
+		}
+	}
+	return result
+}
+
+func getStringBytes(str:String) -> Array<UInt8> {
+	let buf1 = [UInt8](str.utf8)
+	return buf1
+}
+
+func tripSeverity(scanner: Dictionary<Int, Int>) -> Int {
+	var severity = 0
+	for key in scanner.keys {
+		if(key % ((scanner[key]!-1)*2)) == 0 {
+			severity += (key*scanner[key]!)
+		}
+	}
+	return severity
+}
+
+func smallestDelay(scanner: Dictionary<Int, Int>, maxDelay: Int) -> Int {
+	var caught = false
+	for delay in stride(from: 0, to: 10000000, by: 2) {
+		for key in scanner.keys {
+			if((key+delay) % ((scanner[key]!-1)*2)) == 0 {
+				caught = true;
+				break;
+			}
+		}
+		if !caught {
+			return delay
+		}
+		caught = false
+	}
+	return 0
+}
+
+let fileManager = FileManager.default
+let fileDir: String = fileManager.currentDirectoryPath
+let filePath: String = fileDir.appending("/../input.txt")
+let data = fileManager.contents(atPath: filePath)
+let str: String = String(data: data!, encoding: String.Encoding.utf8)!
+
+let inStrArr = readLinesRemoveEmpty(str: str)
+
+var scannerDict = [Int:Int]()
+for s in inStrArr {
+	let items = s.components(separatedBy: .whitespaces).joined().components(separatedBy: [":"])
+	scannerDict[Int(items[0])!] = Int(items[1])!
+}
+
+let start = DispatchTime.now() // <<<<<<<<<< Start time
+print("Trip severity: " + String(tripSeverity(scanner: scannerDict)))
+print("Smallest delay: " + String(smallestDelay(scanner: scannerDict, maxDelay: 10*1000*1000)))
+let end = DispatchTime.now()   // <<<<<<<<<<   end time
+let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
+let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
+print("Time to evaluate problem : \(timeInterval) seconds")
+
