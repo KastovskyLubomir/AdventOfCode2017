6dc98e52ac210f389b0f5d01f3c0321c07717497
Lubomir Kastovsky <lubomir.kastovsky@avg.com>
2017-12-22 00:48:29 +0100

    solved event 21

--- event021/event021/main.swift
+++ event021/event021/main.swift
@@ -6,6 +6,94 @@
 //  Copyright © 2017 Avast. All rights reserved.
 //
 
+/*
+ --- Day 21: Fractal Art ---
+ 
+ You find a program trying to generate some art. It uses a strange process that involves repeatedly enhancing the detail of an image through a set of rules.
+ 
+ The image consists of a two-dimensional square grid of pixels that are either on (#) or off (.). The program always begins with this pattern:
+ 
+ .#.
+ ..#
+ ###
+ Because the pattern is both 3 pixels wide and 3 pixels tall, it is said to have a size of 3.
+ 
+ Then, the program repeats the following process:
+ 
+ If the size is evenly divisible by 2, break the pixels up into 2x2 squares, and convert each 2x2 square into a 3x3 square by following the corresponding enhancement rule.
+ Otherwise, the size is evenly divisible by 3; break the pixels up into 3x3 squares, and convert each 3x3 square into a 4x4 square by following the corresponding enhancement rule.
+ Because each square of pixels is replaced by a larger one, the image gains pixels and so its size increases.
+ 
+ The artist's book of enhancement rules is nearby (your puzzle input); however, it seems to be missing rules. The artist explains that sometimes, one must rotate or flip the input pattern to find a match. (Never rotate or flip the output pattern, though.) Each pattern is written concisely: rows are listed as single units, ordered top-down, and separated by slashes. For example, the following rules correspond to the adjacent patterns:
+ 
+ ../.#  =  ..
+           .#
+ 
+                 .#.
+ .#./..#/###  =  ..#
+                 ###
+ 
+                         #..#
+ #..#/..../#..#/.##.  =  ....
+                         #..#
+                         .##.
+ When searching for a rule to use, rotate and flip the pattern as necessary. For example, all of the following patterns match the same rule:
+ 
+ .#.   .#.   #..   ###
+ ..#   #..   #.#   ..#
+ ###   ###   ##.   .#.
+ Suppose the book contained the following two rules:
+ 
+ ../.# => ##./#../...
+ .#./..#/### => #..#/..../..../#..#
+ As before, the program begins with this pattern:
+ 
+ .#.
+ ..#
+ ###
+ The size of the grid (3) is not divisible by 2, but it is divisible by 3. It divides evenly into a single square; the square matches the second rule, which produces:
+ 
+ #..#
+ ....
+ ....
+ #..#
+ The size of this enhanced grid (4) is evenly divisible by 2, so that rule is used. It divides evenly into four squares:
+ 
+ #.|.#
+ ..|..
+ --+--
+ ..|..
+ #.|.#
+ Each of these squares matches the same rule (../.# => ##./#../...), three of which require some flipping and rotation to line up with the rule. The output for the rule is the same in all four cases:
+ 
+ ##.|##.
+ #..|#..
+ ...|...
+ ---+---
+ ##.|##.
+ #..|#..
+ ...|...
+ Finally, the squares are joined into a new grid:
+ 
+ ##.##.
+ #..#..
+ ......
+ ##.##.
+ #..#..
+ ......
+ Thus, after 2 iterations, the grid contains 12 pixels that are on.
+ 
+ How many pixels stay on after 5 iterations?
+ 
+ Your puzzle answer was 197.
+ 
+ --- Part Two ---
+ 
+ How many pixels stay on after 18 iterations?
+ 
+ Your puzzle answer was 3081737.
+ */
+
 import Foundation
 
 func readLinesRemoveEmpty(str: String) -> Array<String> {
@@ -43,8 +131,6 @@ let str: String = String(data: data!, encoding: String.Encoding.utf8)!
 
 let inputLines = readLinesRemoveEmpty(str: str)
 
-//print(inputLines)
-
 var rules = [String:String]()
 
 for line in inputLines {
@@ -52,30 +138,31 @@ for line in inputLines {
 	rules[args[0]] = args[1]
 }
 
-//print(rules)
-
 let inputPattern = ".#./..#/###"
 
 func rotate(pattern: String) -> String {
-	var rotated = ""
 	if pattern.count <= 5 {
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 3)])
-		rotated += String(pattern[pattern.startIndex]) + "/"
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 4)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 1)])
+        var r = String(pattern[pattern.index(pattern.startIndex, offsetBy: 3)])
+        r.append(pattern[pattern.startIndex])
+        r.append("/")
+        r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 4)])
+        r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 1)])
+        return r
 	}
 	else {
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 8)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 4)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 0)]) + "/"
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 9)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 5)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 1)]) + "/"
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 10)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 6)])
-		rotated += String(pattern[pattern.index(pattern.startIndex, offsetBy: 2)])
+		var r = String(pattern[pattern.index(pattern.startIndex, offsetBy: 8)])
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 4)])
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 0)])
+        r.append("/")
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 9)])
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 5)])
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 1)])
+        r.append("/")
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 10)])
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 6)])
+		r.append(pattern[pattern.index(pattern.startIndex, offsetBy: 2)])
+        return r
 	}
-	return rotated
 }
 
 func flipVertical(pattern: String) -> String {
@@ -90,31 +177,16 @@ func flipVertical(pattern: String) -> String {
 	return flipped
 }
 
-func flipHorizontal(pattern: String) -> String {
-	var flipped = ""
-	var arr: Array<String> = pattern.components(separatedBy: String("/"))
-	if pattern.count <= 5 {
-		flipped = arr[0].reversed() + String("/")
-		flipped += arr[1].reversed()
-	}
-	else {
-		flipped = arr[0].reversed() + "/" + arr[1].reversed() + "/" + arr[2].reversed()
-	}
-	return flipped
-}
-
 func findPatternFits(rules: Dictionary<String,String>, pattern: String) -> String {
 	var pat = pattern
 	for i in 0..<4 {
 		if rules[pat] != nil {
 			return rules[pat]!
 		}
-		if rules[flipVertical(pattern: pat)] != nil {
-			return rules[flipVertical(pattern: pat)]!
-		}
-		if rules[flipHorizontal(pattern: pat)] != nil {
-			return rules[flipHorizontal(pattern: pat)]!
-		}
+        let x = rules[flipVertical(pattern: pat)]
+        if x != nil {
+            return x!
+        }
 		if i <= 2 {
 			pat = rotate(pattern: pat)
 		}
@@ -131,15 +203,12 @@ func getPatterns(matrix: String) -> Array<String> {
 	var arr = [String]()
 	let size = patternSize(pattern: matrix)
 	let m = matrix.components(separatedBy: "/")
+    var x = ""
 	if size % 2 == 0 {
 		for i in 0..<(size/2) {
 			for j in 0..<(size/2) {
-				var a = m[i*2].index(m[i*2].startIndex, offsetBy: j*2)
-				var b = m[i*2].index(m[i*2].startIndex, offsetBy: (j*2)+1)
-				var x = String(m[i*2][a...b]) + "/"
-				a = m[i*2+1].index(m[i*2+1].startIndex, offsetBy: j*2)
-				b = m[i*2+1].index(m[i*2+1].startIndex, offsetBy: (j*2)+1)
-				x += String(m[i*2+1][a...b])
+                x = String(m[i*2][m[i*2].index(m[i*2].startIndex, offsetBy: j*2)...m[i*2].index(m[i*2].startIndex, offsetBy: (j*2)+1)]) + "/"
+				x += String(m[i*2+1][m[i*2+1].index(m[i*2+1].startIndex, offsetBy: j*2)...m[i*2+1].index(m[i*2+1].startIndex, offsetBy: (j*2)+1)])
 				arr.append(x)
 			}
 		}
@@ -148,15 +217,9 @@ func getPatterns(matrix: String) -> Array<String> {
 	if size % 3 == 0 {
 		for i in 0..<(size/3) {
 			for j in 0..<(size/3) {
-				var a = m[i*3].index(m[i*3].startIndex, offsetBy: j*3)
-				var b = m[i*3].index(m[i*3].startIndex, offsetBy: (j*3)+2)
-				var x = String(m[i*3][a...b]) + "/"
-				a = m[i*3+1].index(m[i*3+1].startIndex, offsetBy: j*3)
-				b = m[i*3+1].index(m[i*3+1].startIndex, offsetBy: (j*3)+2)
-				x += String(m[i*3+1][a...b]) + "/"
-				a = m[i*3+2].index(m[i*3+2].startIndex, offsetBy: j*3)
-				b = m[i*3+2].index(m[i*3+2].startIndex, offsetBy: (j*3)+2)
-				x += String(m[i*3+2][a...b])
+				x = String(m[i*3][m[i*3].index(m[i*3].startIndex, offsetBy: j*3)...m[i*3].index(m[i*3].startIndex, offsetBy: (j*3)+2)]) + "/"
+				x += String(m[i*3+1][m[i*3+1].index(m[i*3+1].startIndex, offsetBy: j*3)...m[i*3+1].index(m[i*3+1].startIndex, offsetBy: (j*3)+2)]) + "/"
+				x += String(m[i*3+2][m[i*3+2].index(m[i*3+2].startIndex, offsetBy: j*3)...m[i*3+2].index(m[i*3+2].startIndex, offsetBy: (j*3)+2)])
 				arr.append(x)
 			}
 		}
@@ -173,14 +236,12 @@ func createMatrix(patterns: Array<String>) -> String {
 		return matrix
 	}
 	if sqRoot % 2 == 0 {
+        var line1 = "", line2 = "", line3 = ""
 		for i in 0..<sqRoot {
-			var line1 = ""
-			var line2 = ""
-			var line3 = ""
+            line1 = ""; line2 = ""; line3 = ""
 			for j in 0..<sqRoot {
 				let args = patterns[i*sqRoot+j].components(separatedBy: "/")
-				line1 += args[0]
-				line2 += args[1]
+                line1 += args[0]; line2 += args[1]
 				if args.count == 3 {
 					line3 += args[2]
 				}
@@ -194,16 +255,12 @@ func createMatrix(patterns: Array<String>) -> String {
 		return matrix
 	}
 	if sqRoot % 3 == 0 {
+        var line1 = "", line2 = "", line3 = "", line4 = ""
 		for i in 0..<sqRoot {
-			var line1 = ""
-			var line2 = ""
-			var line3 = ""
-			var line4 = ""
+            line1 = ""; line2 = ""; line3 = ""; line4 = ""
 			for j in 0..<sqRoot {
 				let args = patterns[i*sqRoot+j].components(separatedBy: "/")
-				line1 += args[0]
-				line2 += args[1]
-				line3 += args[2]
+                line1 += args[0]; line2 += args[1]; line3 += args[2]
 				if args.count == 4 {
 					line4 += args[3]
 				}
@@ -228,22 +285,25 @@ func replaceWithPatterns(inputPatterns: Array<String>, rules: Dictionary<String,
 }
 
 var inputArr = [inputPattern]
-//let res = replaceWithPatterns(inputPatterns: inputArr, rules: rules)
-//print(res)
-//let res2 = getPatterns(matrix: res[0])
-//print(res2)
-//let res3 = replaceWithPatterns(inputPatterns: res2, rules: rules)
-//print(res3)
 
-for i in 0..<5 {
+let start = DispatchTime.now() // <<<<<<<<<< Start time
+var N = 16
+for i in 0..<N {
 	print("Iteration: " + String(i))
-	print("Input: " + inputArr.description)
 	let matrixes = replaceWithPatterns(inputPatterns: inputArr, rules: rules)
-	print("Patterns: " + matrixes.description)
 	let matrix = createMatrix(patterns: matrixes)
-	print("Matrix: " + matrix)
-	inputArr = getPatterns(matrix: matrix)
-	print("Result: " + inputArr.description)
-	print("")
+    var counts: [Character: Int] = [:]
+    matrix.forEach { counts[$0, default: 0] += 1 }
+    print(counts)
+    if i < N-1 {
+        inputArr = getPatterns(matrix: matrix)
+    }
+	//print("Result: " + inputArr.description)
+	//print("")
 }
+
+let end = DispatchTime.now()   // <<<<<<<<<<   end time
+let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
+let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
+print("Time to evaluate problem : \(timeInterval) seconds")
 
